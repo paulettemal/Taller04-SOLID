3083e8f2943a87209ada3a841fd7168274156eaa
paulettemal <136779147+paulettemal@users.noreply.github.com>
2024-06-05 15:20:33 -0500

    Archivo

---
+++ Taller04/src/main/java/com/mycompany/taller04/Taller04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller04;
+
+/**
+ *
+ * @author USER
+ */
+public class Taller04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
